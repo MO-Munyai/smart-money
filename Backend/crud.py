a6cba11bff3dc561d8e04b7f886cb8e725ea9cd6
MO-Munyai <kdapreme@gmail.com>
2026-09-09 00:35:13 +0200

    refactor: replace asset CRUD with instrument registry CRUD
    create/list/get-by-ticker/delete for the identity-only registry - no more
    fundamentals fields to persist.

--- Backend/crud.py
+++ Backend/crud.py
@@ -4,30 +4,31 @@ import models
 import schemas
 
 # -------------------------------
-# Assets
+# Instruments (registry)
 # -------------------------------
-def create_asset(db: Session, asset: schemas.AssetCreate):
-    db_asset = models.Asset(
-        ticker=asset.ticker.upper(),
-        name=asset.name,
-        sector=asset.sector,
-        industry=asset.industry,
-        country=asset.country,
-        market_cap=asset.market_cap,
-        pe_ratio=asset.pe_ratio,
-        beta=asset.beta,
-        roe=asset.roe,
-        dividend_yield=asset.dividend_yield
+def create_instrument(db: Session, instrument: schemas.InstrumentCreate):
+    db_instrument = models.Instrument(
+        ticker=instrument.ticker.upper(),
+        type=instrument.type
     )
-    db.add(db_asset)
+    db.add(db_instrument)
     db.commit()
-    db.refresh(db_asset)
-    return db_asset
+    db.refresh(db_instrument)
+    return db_instrument
 
 
-def get_assets(db: Session):
-    return db.query(models.Asset).all()
+def get_instruments(db: Session):
+    return db.query(models.Instrument).all()
 
 
-def get_asset_by_ticker(db: Session, ticker: str):
-    return db.query(models.Asset).filter(models.Asset.ticker == ticker.upper()).first()
+def get_instrument_by_ticker(db: Session, ticker: str):
+    return db.query(models.Instrument).filter(models.Instrument.ticker == ticker.upper()).first()
+
+
+def delete_instrument(db: Session, ticker: str):
+    instrument = get_instrument_by_ticker(db, ticker)
+    if not instrument:
+        return False
+    db.delete(instrument)
+    db.commit()
+    return True
